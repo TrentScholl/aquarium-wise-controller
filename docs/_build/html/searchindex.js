645def9134f3b6c9e6513c6a7a9d129e351ce9b6
Trent Scholl <strent@gmail.com>
2014-09-03 15:37:50 +1000

    Adding RTD

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:42,terms:{content:0,index:0,modul:0,search:0,page:0},objtypes:{},objnames:{},filenames:["index"],titles:["Welcome to Aquarium Wise Controller&#8217;s documentation!"],objects:{},titleterms:{control:0,wise:0,welcom:0,indic:0,aquarium:0,tabl:0,document:0}})
